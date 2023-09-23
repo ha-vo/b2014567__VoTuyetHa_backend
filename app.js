e76933bd6b16a0944d5a07dbfa7ef1acbaea1186
ha-vo <hab2014567@student.ctu.edu.vn>
2023-09-23 08:47:37 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const contactUser = require('./app/routes/contact.route.js')
+const contactRouter = require('./app/routes/contact.route.js')
+const ApiError = require('./app/api-error.js')
 
 const app = express();
 
@@ -12,6 +13,14 @@ app.get('/', (req, res) => {
     res.json({ message: "Wellcome to contact book application" })
 });
 
-app.use('/api/users', contactUser)
+app.use('/api/users', contactRouter)
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "resource not found"))
+})
+
+app.use((err, req, res, next) => {
+    return res.status(err.status || 500).json({ message: err.message || 'internal server error' })
+})
 
 module.exports = app;
