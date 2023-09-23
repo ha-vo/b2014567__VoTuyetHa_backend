1373aefd7b5376e7d5e002266432e861c36ead31
ha-vo <hab2014567@student.ctu.edu.vn>
2023-09-23 08:35:15 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const contactUser = require('./app/routes/contact.route.js')
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
 app.get('/', (req, res) => {
     res.json({ message: "Wellcome to contact book application" })
 });
+
+app.use('/api/users', contactUser)
 
 module.exports = app;
---
+++ app/routes/contact.route.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const contact = require('../controllers/contact.controller.js');
+
+const router = express.Router()
+
+router.route('/')
+    .get(contact.findAll)
+    .post(contact.create)
+    .delete(contact.deleteAll)
+router.route('/favorite')
+    .get(contact.findAllFavorites)
+router.route(":id")
+    .get(contact.findOne)
+    .put(contact.update)
+    .delete(contact.delete)
+
+module.exports = router
